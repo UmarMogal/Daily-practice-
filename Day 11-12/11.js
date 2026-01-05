5635036741ecb4fd03e585467118f039516c1b57
Umar Mogal <your-github-umarmogal74@gmail.com>
2026-01-05 22:38:18 +0530

    new

---
+++ Day 11-12/11.js
@@ -0,0 +1,13 @@
+let age = 20;
+
+if (age >= 18) {
+  console.log("Eligible to vote");
+} else {
+  console.log("Not eligible");
+}
+
+// loop
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
